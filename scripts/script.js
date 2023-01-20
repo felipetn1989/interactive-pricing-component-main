b0c5eaae69d4b385b8223d5658fca193dd0f1986
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-20 13:53:52 -0300

    finished v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+/* Variable declarations */
+
 let planPrice = document.querySelector(".plan_price");
 
 let currentPrice = document.querySelector(".price");
@@ -6,12 +8,16 @@ let period = document.querySelector(".period");
 
 let pageViews = document.querySelector(".page_views");
 
+/* Declaring default values for when the page is loaded for the first time */
+
 islider.value = 1;
 
 let price = 8;
 let priceMultiplier = 1;
 
 toggle.checked = false;
+
+/* Function for when the toggle is changed. It will define the price and text for both the monthly and annual plans. */
 
 toggle.addEventListener("change", function () {
   if (toggle.checked) {
@@ -26,6 +32,8 @@ toggle.addEventListener("change", function () {
     period.innerHTML = `/ month`;
   }
 });
+
+/* This next function will change the price and page views number to the appropriate numbers for each choice of the range element. I used a switch instead of if...else to save some lines of coding. The priceMultiplier has a default value of 1, and then it changes back and forth as the user uses the toggle switch */
 
 islider.addEventListener("change", function () {
   switch (islider.value) {
